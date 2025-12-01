1bc3c73a0fb4e466ec224efc8dc620bbfb6cd754
Nikhil Yadav <nikhildevr.01@gmail.com>
2025-12-01 22:21:08 +0530

    feat: add pricing, checkout, and notification services

---
+++ services/checkout-service/src/main/java/com/rudraksha/shopsphere/checkout/CheckoutApplication.java
@@ -0,0 +1,13 @@
+package com.rudraksha.shopsphere.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CheckoutApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CheckoutApplication.class, args);
+    }
+}
